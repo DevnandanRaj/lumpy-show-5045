64e7aac946a60cbe0b02df2f8dee88e6a91cf440
DevnandanRaj <devnandanraj07@gmail.com>
2023-03-29 20:45:19 +0530

    added react app

---
+++ my-app/src/Components/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import chakraTheme from '@chakra-ui/theme'
+import { ChakraProvider} from '@chakra-ui/react'
+
+
+function LandingPage() {
+  return (
+    <ChakraProvider>
+      <div>
+        {}
+      </div>
+    </ChakraProvider>
+  );
+}
